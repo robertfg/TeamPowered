a2467ce283e32a5d570cd5cba4e95159c7729243
Robert Glover <robert_f_g@hotmail.com>
2018-12-26 23:21:19 -0500

    Reverted to Name

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -8,8 +8,7 @@ namespace TeamPowered
 {
     class Person
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; }
         public string Address { get; set; }
 
 
