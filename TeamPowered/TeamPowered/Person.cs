75413a36c40172ed5f86412d64bc2f8b8414bd25
Robert Glover <robert_f_g@hotmail.com>
2018-12-26 23:22:57 -0500

    Changed to FName, LName

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -8,8 +8,8 @@ namespace TeamPowered
 {
     class Person
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
         public string Address { get; set; }
 
 
